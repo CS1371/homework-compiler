15c749cba466c58642542ad243e00dac2a33d80f
Alexander Rao <alexhrao@gmail.com>
2018-07-14 20:08:43 -0400

    Try different approach

--- GoogleDriveIntegration/src/googleDriveIntegration/logic/GoogleBrowser.java
+++ GoogleDriveIntegration/src/googleDriveIntegration/logic/GoogleBrowser.java
@@ -6,54 +6,54 @@ import javafx.scene.Scene;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.FileReader;
 
 public class GoogleBrowser {
-    private String fileId = null;
+    private String folderId = null;
 
+    /**
+     * GoogleBrowser shows a prompt to the user, in order to retrieve a folder
+     * @param prompt The prompt to show the user (i.e., what should they select?)
+     * @throws Exception
+     */
     public GoogleBrowser(String prompt) throws Exception {
         // Create Stage
         Parent root = FXMLLoader.load(getClass().getResource("../resources/view.fxml"));
         Stage stage = new Stage();
         stage.setTitle("Google Drive Browser");
         stage.setScene(new Scene(root));
-        WebView viewer = (WebView)root.getChildrenUnmodifiable().get(0);
-        viewer.getEngine().setJavaScriptEnabled(true);
-        BufferedReader reader = new BufferedReader(
-                new FileReader(getClass().getResource("../resources/index.html").getFile())
-        );
-        StringBuilder builder = new StringBuilder();
-        String line;
-        while ((line = reader.readLine()) != null) {
-            builder.append(line);
-        }
-        viewer.getEngine().loadContent(builder.toString());
-        viewer.getEngine().setOnAlert(event -> alertHandler(event.getData()));
-
         stage.showAndWait();
-    }
-
-    private void alertHandler(String data) {
-        if (data != null && data != "") {
-            this.fileId = data;
-        }
-    }
-
-    public String getFileId() throws Exception {
-        if (this.fileId == null) {
+        // if null, then we didn't get anything; die
+        if (this.folderId == null) {
             throw new Exception();
-        } else {
-            return this.fileId;
         }
     }
 
-    public void downloadFolder(String ) {
+    /**
+     * downloadFolder will download the selected folder to the given destination
+     *
+     * If the folder name already exists in the destination, then it is first deleted.
+     * Google Doc formatted files will be converted to PDFs on download; no other conversion is done.
+     * @param path The path to the local folder destination
+     */
+    public void downloadFolderContents(String path) {
 
     }
 
-    public void uploadFolder() {
+    /**
+     * uploadFolder will upload the given path to Google Drive.
+     *
+     * If path points to a single file, that file is uploaded to the
+     * folder previously selected; if it points to a directory, then
+     * all the contents of that folder are uploaded, _including_ the
+     * folder itself. So, if the folder is named myFolder, then under
+     * the selected folder a new folder called myFolder will exist.
+     * If myFolder doesn't exist, it is created; if it does, then myFolder
+     * is first deleted.
+     * @param path The path to the resource
+     */
+    public void uploadFolder(String path) {
 
     }
 }
