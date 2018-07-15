bcd7d044e7d54b85085ea8b21f7bd90a6a9806be
dprofili <dprofili@gmail.com>
2018-07-15 15:02:33 -0400

    Add basic ui layout

--- TestCaseCompiler/TestCaseCompilerController.java
+++ TestCaseCompiler/TestCaseCompilerController.java
@@ -9,19 +9,17 @@ import javafx.scene.control.Button;
  * @version 1.0
  */
 public class TestCaseCompilerController {
-    @FXML
-    private Button testButton;
-    boolean buttonPressed;
+//    private Button testButton;
+//    boolean buttonPressed;
 
-    @FXML
     protected void handleTestButtonPress(ActionEvent e) {
         System.out.println("Button was pressed");
         System.out.println(e.getSource());
-        if (!buttonPressed) {
-            testButton.setText("Fuck off");
-            buttonPressed = true;
-        } else {
-            testButton.setText("You already pressed the button, go to hell and die");
-        }
+//        if (!buttonPressed) {
+//            testButton.setText("Fuck off");
+//            buttonPressed = true;
+//        } else {
+//            testButton.setText("You already pressed the button, go to hell and die");
+//        }
     }
 }
