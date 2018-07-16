05689f4d621d105fa7044d49de33194a5820f053
dprofili <dprofili@gmail.com>
2018-07-16 01:51:28 -0400

    Add folder chooser for local disk

--- TestCaseCompiler/src/controller/TestCaseCompilerController.java
+++ TestCaseCompiler/src/controller/TestCaseCompilerController.java
@@ -1,8 +1,17 @@
 package controller;
-
 import javafx.fxml.FXML;
-import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.Tab;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+import javafx.stage.DirectoryChooser;
+import javafx.event.ActionEvent;
+
+import java.io.File;
 
 /**
  * Controller class for the main test case compiler gui.
@@ -11,17 +20,59 @@ import javafx.scene.control.Button;
  * @version 1.0
  */
 public class TestCaseCompilerController {
-//    private Button testButton;
-//    boolean buttonPressed;
+    @FXML
+    private CheckBox driveCheckbox;
 
-    protected void handleTestButtonPress(ActionEvent e) {
-        System.out.println("Button was pressed");
-        System.out.println(e.getSource());
-//        if (!buttonPressed) {
-//            testButton.setText("Fuck off");
-//            buttonPressed = true;
-//        } else {
-//            testButton.setText("You already pressed the button, go to hell and die");
-//        }
+    @FXML
+    private CheckBox localCheckbox;
+
+    @FXML
+    private Button remoteButton;
+
+    @FXML
+    private Button localButton;
+
+    @FXML
+    private Tab studentTab;
+
+    @FXML
+    private Tab submissionTab;
+
+    @FXML
+    private AnchorPane resubmissionTab;
+
+    @FXML
+    private Menu HelpMenu;
+
+    @FXML
+    private MenuItem HelpMenuItem;
+
+    @FXML
+    private MenuItem AboutMenuItem;
+
+    @FXML
+    private AnchorPane statusBarAnchorPane;
+
+    // UI specific instance fields
+    private File localOutputDirectory;
+    @FXML
+    void driveCheckboxClicked(ActionEvent event) {
+        remoteButton.setDisable(!driveCheckbox.isSelected());
+    }
+
+    @FXML
+    void localCheckboxClicked(ActionEvent event) {
+        localButton.setDisable(!localCheckbox.isSelected());
+    }
+
+    @FXML
+    void localButtonPressed(ActionEvent event) {
+        // Open a directory chooser dialog to pick the local save folder
+        DirectoryChooser fc = new DirectoryChooser();
+        fc.setTitle("Pick local output folder");
+        File selected = fc.showDialog(localButton.getScene().getWindow());
+        if (selected != null) {
+            localOutputDirectory = selected;
+        }
     }
 }
