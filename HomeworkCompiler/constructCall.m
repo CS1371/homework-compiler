9a883b2ca3c754f4a66ad8a61de4fca2086a66ac
Alexander Rao <alexhrao@gmail.com>
2019-02-25 00:05:38 -0500

    Fix bug where no outputs would be expected

--- HomeworkCompiler/constructCall.m
+++ HomeworkCompiler/constructCall.m
@@ -4,5 +4,11 @@
 % Outputs O into function call F.
 %
 function call = constructCall(name, ins, outs)
+    if ~iscell(outs) && isempty(outs)
+        call = [name '(' strjoin(ins, ', ') ')'];
+        return;
+    elseif ~iscell(outs)
+        outs = {outs};
+    end
     call = ['[' strjoin(outs, ', ') '] = ' name '(' strjoin(ins, ', ') ')'];
 end
