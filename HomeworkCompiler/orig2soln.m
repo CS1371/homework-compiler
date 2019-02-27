33b083bfea0f3c0ea31d70d91facd7bcaad87d84
Alexander Rao <alexhrao@gmail.com>
2019-02-27 14:45:41 -0500

    Make sure orig2soln handles no outputs

--- HomeworkCompiler/orig2soln.m
+++ HomeworkCompiler/orig2soln.m
@@ -12,14 +12,20 @@
 function solnCall = orig2soln(studCall)
     % will ALWAYS be of form:
     % [OUTPUT1, OUTPUT2, ...] = call(IN1, IN2, ...)
-    
+    % or no outputs
     % get outputs
-    outs = studCall(2:strfind(studCall, ']') - 1);
-    ins = studCall(strfind(studCall, '(') + 1:strfind(studCall, ')') - 1);
-    call = studCall(strfind(studCall, '=') + 2:strfind(studCall, '(') - 1);
-    if ~isempty(outs)
+    % if no [], no outputs!
+    if ~contains(studCall, ']')
+        % doesn't have outputs
+        ins = studCall(strfind(studCall, '(') + 1:strfind(studCall, ')') - 1);
+        call = studCall(1:strfind(studCall, '(') - 1);
+        solnCall = [call '_soln(' ins ')'];
+    else
+       outs = studCall(2:strfind(studCall, ']') - 1);
+        ins = studCall(strfind(studCall, '(') + 1:strfind(studCall, ')') - 1);
+        call = studCall(strfind(studCall, '=') + 2:strfind(studCall, '(') - 1);
         outs = strsplit(outs, ', ');
         outs = [strjoin(outs, '_soln, ') '_soln'];
+        solnCall = ['[' outs '] = ' call '_soln(' ins ')']; 
     end
-    solnCall = ['[' outs '] = ' call '_soln(' ins ')'];
 end
